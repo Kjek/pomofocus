d7a62a14c3b6d2a5f87fa294422a15b21697272a
Kjek <kjek@me.com>
2024-01-09 14:49:44 +0100

    Added some CSS

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,11 @@ function App() {
     <>
       <div className="w-screen">
         <div className="flex flex-col w-1/2 gap-2 m-auto">
-          <span className="w-full text-center">{formatedTime}</span>
+          <span className="w-full text-center dark:text-gray-300">
+            {formatedTime}
+          </span>
           <input
-            className="w-full border-2 border-black rounded"
+            className="w-full border-2 active:bg-gray-100 border-gray-300 rounded-lg dark:text-gray-300 dark:border-gray-600 dark:hover:bg-gray-700 dark:active:bg-gray-600 dark:hover:text-white hover:bg-gray-200 cursor-pointer"
             type="button"
             value="Play/Pause"
             onClick={toggleTimer}
