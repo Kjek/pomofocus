806a0081fb55b3c84d684b4a2b9cdd8f9da8a6e0
Kjek <kjek@me.com>
2024-01-09 14:57:47 +0100

    Added prettierrc

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import useTimer from '@hook/useTimer';
+import useTimer from '@hook/useTimer'
 
 function App() {
-  const { toggleTimer, formatedTime } = useTimer();
+  const { toggleTimer, formatedTime } = useTimer()
 
   return (
     <>
-      <div className="w-screen">
-        <div className="flex flex-col w-1/2 gap-2 m-auto">
-          <span className="w-full text-center dark:text-gray-300">
+      <div className='w-screen'>
+        <div className='m-auto flex w-1/2 flex-col gap-2'>
+          <span className='w-full text-center text-gray-800 dark:text-gray-300'>
             {formatedTime}
           </span>
           <input
-            className="w-full border-2 active:bg-gray-100 border-gray-300 rounded-lg dark:text-gray-300 dark:border-gray-600 dark:hover:bg-gray-700 dark:active:bg-gray-600 dark:hover:text-white hover:bg-gray-200 cursor-pointer"
-            type="button"
-            value="Play/Pause"
+            className='w-full cursor-pointer rounded-lg border-2 border-gray-300 text-gray-800 hover:bg-gray-200 active:bg-gray-100 dark:border-gray-600 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-white dark:active:bg-gray-600'
+            type='button'
+            value='Play/Pause'
             onClick={toggleTimer}
           />
         </div>
       </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
