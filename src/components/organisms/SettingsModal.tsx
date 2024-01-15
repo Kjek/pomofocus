2ffbf59373d49cae342fba18a7af18106a07f0f6
Kjek <kjek@me.com>
2024-01-15 13:56:45 +0100

    Fixed settings UX

--- src/components/organisms/SettingsModal.tsx
+++ src/components/organisms/SettingsModal.tsx
@@ -17,30 +17,33 @@ const SettingsModal = (props: SettingsModalProps) => {
     <>
       <SettingsButton onClick={toggleOpen} />
       {isOpen ? (
-        <div className='absolute left-0 top-0 h-screen w-screen border-spacing-6 pt-4'>
-          <div className='m-auto flex w-fit flex-col gap-6 p-4 dark:bg-gray-800'>
-            <div className='flex'>
-              <Text variant='h3' className='flex-grow self-center'>
-                Settings
-              </Text>
-              <CancelButton className='self-end' onClick={toggleOpen} />
+        <>
+          <div className='absolute left-0 top-0 h-screen w-screen bg-black opacity-30' />
+          <div className='fixed border-spacing-6 self-center pt-4 sm:pt-6'>
+            <div className='m-auto flex w-fit flex-col gap-6 rounded-lg border border-gray-300 bg-white p-4 dark:border-gray-600 dark:bg-gray-800'>
+              <div className='flex'>
+                <Text variant='h3' className='flex-grow self-center'>
+                  Settings
+                </Text>
+                <CancelButton className='self-end' onClick={toggleOpen} />
+              </div>
+              <ul className='flex flex-col gap-4'>
+                {settings.map((setting, index) => {
+                  return (
+                    <li className='flex' key={index}>
+                      <SettingsOption
+                        storageKey={setting.storageKey}
+                        defaultValue={setting.defaultValue}
+                        name={setting.name}
+                        options={setting.options}
+                      />
+                    </li>
+                  );
+                })}
+              </ul>
             </div>
-            <ul className='flex flex-col gap-4'>
-              {settings.map((setting, index) => {
-                return (
-                  <li className='flex' key={index}>
-                    <SettingsOption
-                      storageKey={setting.storageKey}
-                      defaultValue={setting.defaultValue}
-                      name={setting.name}
-                      options={setting.options}
-                    />
-                  </li>
-                );
-              })}
-            </ul>
           </div>
-        </div>
+        </>
       ) : null}
     </>
   );
